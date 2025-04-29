f9f9aef8572439376bdc7645688fa31c5f5f5c16
Buck McCready <jbuckmccready@gmail.com>
2025-04-29 16:32:38 -0700

    feat(mkdnflow): improvements to config
    - Disable some unneeded modules (e.g., folds wasn't working with ufo).
    - Set perspective for links to "current" for current file.

--- nvim/.config/nvim/lua/plugins/mkdnflow.lua
+++ nvim/.config/nvim/lua/plugins/mkdnflow.lua
@@ -1,16 +1,35 @@
 return {
     "jakewvincent/mkdnflow.nvim",
     ft = "markdown",
-    config = function()
-        require("mkdnflow").setup({
-            mappings = {
-                -- turn on insert mode MkdnEnter for auto list bullet creation on <CR>
-                MkdnEnter = { { "n", "v", "i" }, "<CR>" },
-                MkdnFoldSection = false,
-                MkdnUnfoldSection = false,
-                MkdnTableNextCell = false,
-                MkdnTablePrevCell = false,
-            },
-        })
-    end,
+    opts = {
+        modules = {
+            bib = false,
+            buffers = false,
+            conceal = true,
+            cursor = true,
+            folds = false,
+            links = true,
+            lists = true,
+            maps = true,
+            paths = true,
+            tables = true,
+            yaml = false,
+            cmp = false,
+        },
+        perspective = {
+            priority = "current",
+            fallback = "current",
+            root_tell = false,
+            nvim_wd_heel = false,
+            update = false,
+        },
+        mappings = {
+            -- turn on insert mode MkdnEnter for auto list bullet creation on <CR>
+            MkdnEnter = { { "n", "v", "i" }, "<CR>" },
+            MkdnFoldSection = false,
+            MkdnUnfoldSection = false,
+            MkdnTableNextCell = false,
+            MkdnTablePrevCell = false,
+        },
+    },
 }
