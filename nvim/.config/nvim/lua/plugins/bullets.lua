7d326f08f2c172a053540498310151312fd28d73
Buck McCready <jbuckmccready@gmail.com>
2025-04-30 16:34:05 -0700

    feat(bullets.vim): fixup config
    - Add bullets toggle keymap (was done by mkdnflow before).
    - Define checkbox markers.

--- nvim/.config/nvim/lua/plugins/bullets.lua
+++ nvim/.config/nvim/lua/plugins/bullets.lua
@@ -2,11 +2,13 @@ return {
     "bullets-vim/bullets.vim",
     lazy = false,
     init = function()
-        -- just using for new line bullet creation and
         vim.g.bullets_set_mappings = 0
+        vim.g.bullets_checkbox_markers = " -x"
+
         vim.g.bullets_custom_mappings = {
             { "imap", "<cr>", "<Plug>(bullets-newline)" },
             { "nmap", "o", "<Plug>(bullets-newline)" },
+            { "nmap", "<C-Space>", "<Plug>(bullets-toggle-checkbox)" },
             { "vmap", "<leader>mR", "<Plug>(bullets-renumber)" },
             { "nmap", "<leader>mR", "<Plug>(bullets-renumber)" },
         }
