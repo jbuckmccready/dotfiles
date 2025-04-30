ced5b1de59b80c541cde8595c118cf3a81f53ee7
Buck McCready <jbuckmccready@gmail.com>
2025-04-30 13:53:37 -0700

    feat(keymappings): simplify autoscrolling to just jk
    - `scrolloff` was annoying with basic searches.
    - autocmd to handle end of file had issues with going between modes,
      which was fixable but search interaction was reason enough to
      simplify.

--- nvim/.config/nvim/lua/config/keymappings.lua
+++ nvim/.config/nvim/lua/config/keymappings.lua
@@ -4,6 +4,22 @@ vim.keymap.set("n", "<C-u>", "<C-u>zz", { desc = "Scroll upwards" })
 vim.keymap.set("n", "n", "nzzzv", { desc = "Next result" })
 vim.keymap.set("n", "N", "Nzzzv", { desc = "Previous result" })
 
+-- jk autoscrolling
+vim.keymap.set("n", "j", function()
+    if vim.g.jk_auto_scroll then
+        vim.cmd("normal! jzz")
+    else
+        vim.cmd("normal! j")
+    end
+end, { noremap = true, desc = "Cursor down" })
+vim.keymap.set("n", "k", function()
+    if vim.g.jk_auto_scroll then
+        vim.cmd("normal! kzz")
+    else
+        vim.cmd("normal! k")
+    end
+end, { noremap = true, desc = "Cursor up" })
+
 -- Indent while remaining in visual mode
 vim.keymap.set("v", "<", "<gv")
 vim.keymap.set("v", ">", ">gv")
@@ -93,18 +109,14 @@ vim.keymap.set({ "n" }, "\\f", function()
     vim.notify(string.format("%s formatting...", vim.g.autoformat and "Enabling" or "Disabling"), vim.log.levels.INFO)
 end, { desc = "Toggle formatting" })
 
--- Toggle scrolloff
+-- Toggle jk auto scrolling
 vim.keymap.set({ "n" }, "\\j", function()
-    vim.o.scrolloff = vim.o.scrolloff == 0 and 999 or 0
+    vim.g.jk_auto_scroll = not vim.g.jk_auto_scroll
     vim.notify(
-        string.format(
-            "%s auto scrolling (scrolloff = %s)...",
-            vim.o.scrolloff == 0 and "Disabling" or "Enabling",
-            vim.o.scrolloff
-        ),
+        string.format("%s auto scrolling with jk...", vim.g.jk_auto_scroll and "Enabling" or "Disabling"),
         vim.log.levels.INFO
     )
-end, { desc = "Toggle auto scrolling (scrolloff)" })
+end, { desc = "Toggle auto scrolling with jk" })
 
 -- Toggle ignore white space for diffs
 vim.keymap.set({ "n" }, "<leader>gi", function()
