57b4cc2cb1ab0d3572bfe6e665e1d22d2660ae58
Buck McCready <jbuckmccready@gmail.com>
2025-04-30 11:01:47 -0700

    fix(nvim): update patches

--- nvim/.config/nvim/lua/config/patches.lua
+++ nvim/.config/nvim/lua/config/patches.lua
@@ -1,13 +1,26 @@
--- FIXME: workaround for https://github.com/neovim/neovim/issues/28058
--- Error pops up when opening .go files without this workaround
 local make_client_capabilities = vim.lsp.protocol.make_client_capabilities
 function vim.lsp.protocol.make_client_capabilities()
     local caps = make_client_capabilities()
-    if not (caps.workspace or {}).didChangeWatchedFiles then
-        vim.notify("lsp capability didChangeWatchedFiles is already disabled", vim.log.levels.WARN)
-    else
-        caps.workspace.didChangeWatchedFiles = nil
-    end
+
+    -- FIXME: workaround for https://github.com/neovim/neovim/issues/28058
+    -- Error pops up when opening .go files without this workaround
+    -- NOTE: this workaround no longer seems to work with neovim 0.11.0 or 0.11.1 - commented out for now
+    -- if not (caps.workspace or {}).didChangeWatchedFiles then
+    --     vim.notify("lsp capability didChangeWatchedFiles is already disabled", vim.log.levels.WARN)
+    -- else
+    --     caps.workspace.didChangeWatchedFiles = nil
+    -- end
+
+    -- HACK:
+    -- Setup capabilities to support utf-16, since copilot.vim only works with utf-16
+    -- this is a workaround to the limitations of copilot language server
+    -- Related issue: https://github.com/neovim/nvim-lspconfig/issues/2184
+    caps = vim.tbl_deep_extend("force", caps, {
+        offsetEncoding = { "utf-16" },
+        general = {
+            positionEncodings = { "utf-16" },
+        },
+    })
 
     return caps
 end
