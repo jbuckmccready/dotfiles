3bb19bd34a4c41ad6348102592f78fa78c2f904e
Buck McCready <jbuckmccready@gmail.com>
2025-05-02 23:43:51 -0700

    chore(lualine): move lualine setup into plugin file

--- nvim/.config/nvim/lua/plugins/lualine.lua
+++ nvim/.config/nvim/lua/plugins/lualine.lua
@@ -1,4 +1,46 @@
 return {
     "nvim-lualine/lualine.nvim",
     dependencies = { "nvim-tree/nvim-web-devicons" },
+    opts = {
+        options = {
+            icons_enabled = true,
+            theme = "auto",
+            component_separators = "|",
+            section_separators = "",
+            disabled_filetypes = {
+                statusline = {},
+                winbar = {},
+            },
+            ignore_focus = {},
+            always_divide_middle = true,
+            globalstatus = false,
+            refresh = {
+                statusline = 500,
+                tabline = 500,
+                winbar = 500,
+            },
+        },
+        sections = {
+            lualine_a = { "mode" },
+            lualine_b = { "branch", "diff", "diagnostics" },
+            -- path = 2 for absolute file path
+            lualine_c = { { "filename", path = 2 } },
+            lualine_x = { "encoding", "fileformat", "filetype" },
+            lualine_y = { "progress" },
+            lualine_z = { "location" },
+        },
+        inactive_sections = {
+            lualine_a = {},
+            lualine_b = {},
+            lualine_c = { "filename" },
+            lualine_d = {},
+            lualine_x = { "location" },
+            lualine_y = {},
+            lualine_z = {},
+        },
+        tabline = {},
+        winbar = {},
+        inactive_winbar = {},
+        extensions = {},
+    },
 }
