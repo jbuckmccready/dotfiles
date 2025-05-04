32ae5561e8933092a9d69c7c7ab0f6660f019820
Buck McCready <jbuckmccready@gmail.com>
2025-05-03 21:19:51 -0700

    feat(copilot): use right arrow for accept completion
    Avoids conflict with tab for jump to next snippet position, allowing use
    of copilot autocomplete for snippet entry before jumping to next place in
    snippet.
    
    This also matches the default ghost text accept in fish shell.

--- nvim/.config/nvim/lua/plugins/copilot.lua
+++ nvim/.config/nvim/lua/plugins/copilot.lua
@@ -1,6 +1,13 @@
 return {
     "github/copilot.vim",
     lazy = false,
+    init = function()
+        vim.keymap.set("i", "<Right>", 'copilot#Accept("\\<CR>")', {
+            expr = true,
+            replace_keycodes = false,
+        })
+        vim.g.copilot_no_tab_map = true
+    end,
     keys = {
         { "<leader>cp", "<cmd>Copilot panel<CR>", desc = "Copilot Suggestions Panel" },
         { "<C-f>", "<Plug>(copilot-next)", mode = "i", desc = "Next Copilot Suggestion" },
