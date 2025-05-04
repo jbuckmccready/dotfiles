7f742bcf501e09c40e3688726470bfe214406872
Buck McCready <jbuckmccready@gmail.com>
2025-05-03 17:02:35 -0700

    feat(shell): use zsh as parent process for fish shell
    - Avoids compatibility problems, ensures exports/path setup in zsh
      config is applied.
    - Removed anything not environment related from .zshrc and setup to
      immediately enter fish shell.
    - Removed environment related things from fish config.
    - Changed aliases to use fish abbr instead and added `nv` abbr.

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,12 +1,16 @@
-# setup environment variables and PATH
-source $__fish_config_dir/environment.fish
+
+# Nothing to do if not running interactively
+if not status is-interactive
+	return 0
+end
+
+# Ghostty's shell integration
+if test -n "$GHOSTTY_RESOURCES_DIR"
+    source $GHOSTTY_RESOURCES_DIR/shell-integration/fish/vendor_conf.d/ghostty-shell-integration.fish
+end
 
 # Color scheme
 fish_config theme choose "Catppuccin Mocha"
-
-if status is-interactive
-	# Commands to run in interactive sessions can go here
-end
 
 # Hydro prompt settings
 set -g hydro_symbol_start \n
@@ -17,31 +21,22 @@ set -g hydro_color_duration $fish_color_gray
 set -g hydro_symbol_git_dirty "*"
 set -g hydro_multiline true
 
+
+abbr -a nv "nvim"
 # fzf open to edit in neovim
-alias fzfe="fzf --multi --bind 'enter:become(nvim {+})'"
+abbr -a fzfe "fzf --multi --bind 'enter:become(nvim {+})'"
 
 # Launch lazygit with default config
-alias lg="lazygit"
-
+abbr -a lg "lazygit"
 # Launch lazygit with config setup to use difftastic
-alias lgt="lazygit --use-config-file ~/.config/lazygit/difftastic-config.yml"
+abbr -a lgt "lazygit --use-config-file ~/.config/lazygit/difftastic-config.yml"
 
 # Remove the greeting message.
 set -U fish_greeting
 
 # zoxide integration
 zoxide init fish | source
-alias cd="z"
+abbr -a cd "z"
 
 # fzf shell integration
-# fzf cattpuccin colors
-set -gx FZF_DEFAULT_OPTS "\
---color=bg+:#313244,bg:#1e1e2e,spinner:#f5e0dc,hl:#f38ba8 \
---color=fg:#cdd6f4,header:#f38ba8,info:#cba6f7,pointer:#f5e0dc \
---color=marker:#b4befe,fg+:#cdd6f4,prompt:#cba6f7,hl+:#f38ba8 \
---color=selected-bg:#45475a \
---color=border:#313244,label:#cdd6f4"
 fzf --fish | source
-
-# neovim for kubectl/k9s
-export KUBE_EDITOR=nvim
--- zsh/.zshrc
+++ zsh/.zshrc
@@ -1,35 +1,10 @@
-autoload -Uz compinit
-compinit
-
-# zoxide setup
-eval "$(zoxide init zsh)"
-alias cd="z"
-
-# Set up fzf key bindings and fuzzy completion
+## Fzf color scheme
 export FZF_DEFAULT_OPTS=" \
 --color=bg+:#313244,bg:#1e1e2e,spinner:#f5e0dc,hl:#f38ba8 \
 --color=fg:#cdd6f4,header:#f38ba8,info:#cba6f7,pointer:#f5e0dc \
 --color=marker:#b4befe,fg+:#cdd6f4,prompt:#cba6f7,hl+:#f38ba8 \
 --color=selected-bg:#45475a \
 --multi"
-source <(fzf --zsh)
-
-# added for ls colors on macos
-export CLICOLOR=1
-
-# init oh my posh prompt
-if [ "$TERM_PROGRAM" != "Apple_Terminal" ]; then
-  eval "$(oh-my-posh init zsh --config ~/.config/ohmyposh/zen.toml)"
-fi
-
-# fzf open to edit in neovim
-alias fzfe="fzf --multi --bind 'enter:become(nvim {+})'"
-
-# Launch lazygit with default config
-alias lg="lazygit"
-
-# Launch lazygit with config setup to use difftastic
-alias lgt="lazygit --use-config-file ~/.config/lazygit/difftastic-config.yml"
 
 # neovim for kubectl/k9s
 export KUBE_EDITOR=nvim
@@ -57,3 +32,10 @@ export GOROOT="/opt/homebrew/opt/go@1.22/libexec"
 export PATH="/opt/homebrew/opt/go@1.22/libexec/bin:$PATH"
 
 . "$HOME/.local/bin/env"
+
+# Execute fish if it's not the parent process.
+if ! ps -p $PPID | grep -q fish; then
+  fish
+  # automatically exit zsh once fish exits
+  exit
+fi
