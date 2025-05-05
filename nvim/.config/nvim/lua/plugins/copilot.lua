82b636001b0650ae0f5e63112a4ddb79d379110e
Buck McCready <jbuckmccready@gmail.com>
2025-05-04 19:09:27 -0700

    fix(copilot): revert copilot accept completion to `<Tab>`
    Turns out I use right arrow while editing quite a bit.

--- nvim/.config/nvim/lua/plugins/copilot.lua
+++ nvim/.config/nvim/lua/plugins/copilot.lua
@@ -1,13 +1,6 @@
 return {
     "github/copilot.vim",
     lazy = false,
-    init = function()
-        vim.keymap.set("i", "<Right>", 'copilot#Accept("\\<CR>")', {
-            expr = true,
-            replace_keycodes = false,
-        })
-        vim.g.copilot_no_tab_map = true
-    end,
     keys = {
         { "<leader>cp", "<cmd>Copilot panel<CR>", desc = "Copilot Suggestions Panel" },
         { "<C-f>", "<Plug>(copilot-next)", mode = "i", desc = "Next Copilot Suggestion" },
