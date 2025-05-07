b493a1389ef4093dee8670705d6373e19607ca93
Buck McCready <jbuckmccready@gmail.com>
2025-05-07 12:21:10 -0700

    chore(nvim): move rustaceanvim global config to plugin file

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -123,35 +123,3 @@ require("mason-lspconfig").setup({
         "zls",
     },
 })
-
--- rust lsp
-vim.g.rustaceanvim = {
-    -- Plugin configuration
-    tools = {
-        -- Don't run clippy on save
-        enable_clippy = false,
-    },
-    -- LSP configuration
-    server = {
-        on_attach = {},
-        capabilities = {},
-        load_vscode_settings = false,
-        default_settings = {
-            -- rust-analyzer language server configuration
-            ["rust-analyzer"] = {
-                rustfmt = {
-                    -- nightly rust fmt
-                    extraArgs = { settings.rust_fmt_extra_args },
-                },
-                -- increase limit to 1024 for searching across workspace (defaults to only 128)
-                workspace = { symbol = { search = { limit = 1024 } } },
-                cargo = {
-                    -- enable all feature flags for opened project
-                    features = "all",
-                },
-            },
-        },
-    },
-    -- DAP configuration
-    dap = {},
-}
--- nvim/.config/nvim/lua/plugins/rustaceanvim.lua
+++ nvim/.config/nvim/lua/plugins/rustaceanvim.lua
@@ -2,4 +2,36 @@ return {
     "mrcjkb/rustaceanvim",
     version = "^6",
     lazy = false, -- This plugin is already lazy
+    init = function()
+        vim.g.rustaceanvim = {
+            -- Plugin configuration
+            tools = {
+                -- Don't run clippy on save
+                enable_clippy = false,
+            },
+            -- LSP configuration
+            server = {
+                on_attach = {},
+                capabilities = {},
+                load_vscode_settings = false,
+                default_settings = {
+                    -- rust-analyzer language server configuration
+                    ["rust-analyzer"] = {
+                        rustfmt = {
+                            -- nightly rust fmt
+                            extraArgs = { require("config.settings").rust_fmt_extra_args },
+                        },
+                        -- increase limit to 1024 for searching across workspace (defaults to only 128)
+                        workspace = { symbol = { search = { limit = 1024 } } },
+                        cargo = {
+                            -- enable all feature flags for opened project
+                            features = "all",
+                        },
+                    },
+                },
+            },
+            -- DAP configuration
+            dap = {},
+        }
+    end,
 }
