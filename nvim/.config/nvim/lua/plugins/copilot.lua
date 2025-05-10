6900a7ddd1f05cbed3f1082df1acac1d22b4c834
Buck McCready <jbuckmccready@gmail.com>
2025-05-10 14:55:55 -0700

    feat(copilot): adjust config to always use copilot in codecompanion

--- nvim/.config/nvim/lua/plugins/copilot.lua
+++ nvim/.config/nvim/lua/plugins/copilot.lua
@@ -28,7 +28,26 @@ return {
                 markdown = true,
                 gitcommit = true,
                 gitrebase = true,
+                -- Add codecompanion filetype
+                codecompanion = true,
             },
+            -- override default should_attach function for codecompanion
+            should_attach = function(_, _)
+                -- override to always attach to codecompanion (despite vim.bo.buflisted == false)
+                if vim.bo.filetype == "codecompanion" then
+                    return true
+                end
+
+                if not vim.bo.buflisted then
+                    return false
+                end
+
+                if vim.bo.buftype ~= "" then
+                    return false
+                end
+
+                return true
+            end,
         })
     end,
 }
