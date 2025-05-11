266b6f2c4f8029867c33ad734138b5fb3a2bd670
Buck McCready <jbuckmccready@gmail.com>
2025-05-10 18:54:47 -0700

    feat(treesitter): add incremental selection and more languages to ensure_installed

--- nvim/.config/nvim/lua/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -1,23 +1,44 @@
 return {
     "nvim-treesitter/nvim-treesitter",
-    config = function()
-        require("nvim-treesitter.configs").setup({
-            highlight = {
-                enable = true,
+    event = { "BufReadPre", "BufNewFile" },
+    build = ":TSUpdate",
+    opts = {
+        highlight = {
+            enable = true,
+        },
+        ensure_installed = {
+            "bash",
+            "c",
+            "cpp",
+            "diff",
+            "go",
+            "html",
+            "javascript",
+            "json",
+            "lua",
+            "luadoc",
+            "markdown",
+            "markdown_inline",
+            "python",
+            "rust",
+            "sql",
+            "terraform",
+            "vim",
+            "vimdoc",
+            "yaml",
+            "zig",
+        },
+        incremental_selection = {
+            enable = true,
+            keymaps = {
+                init_selection = "<cr>",
+                node_incremental = "<cr>",
+                scope_incremental = false,
+                node_decremental = "<bs>",
             },
-            ensure_installed = {
-                "bash",
-                "c",
-                "cpp",
-                "go",
-                "lua",
-                "luadoc",
-                "markdown",
-                "markdown_inline",
-                "rust",
-                "vim",
-                "vimdoc",
-            },
-        })
+        },
+    },
+    config = function(_, opts)
+        require("nvim-treesitter.configs").setup(opts)
     end,
 }
