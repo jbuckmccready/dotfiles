fde1b474d7e27718505b3fae1b5fbdb18891fcf6
Buck McCready <jbuckmccready@gmail.com>
2025-05-24 14:32:36 -0700

    fix(bullets): use only `-` for lists

--- nvim/.config/nvim/lua/plugins/bullets.lua
+++ nvim/.config/nvim/lua/plugins/bullets.lua
@@ -2,8 +2,9 @@ return {
     "bullets-vim/bullets.vim",
     lazy = false,
     init = function()
-        vim.g.bullets_set_mappings = 0
-        vim.g.bullets_checkbox_markers = " -x"
+        vim.g.bullets_set_mappings = 0 -- disable default mappings
+        vim.g.bullets_checkbox_markers = " -x" -- lowercase x
+        vim.g.bullets_outline_levels = { "ROM", "ABC", "num", "abc", "rom", "std-" } -- use only `-` for lists
 
         vim.g.bullets_custom_mappings = {
             { "imap", "<cr>", "<Plug>(bullets-newline)" },
