7357df228bdb7636c6b5e7026c32bde87ac70058
Buck McCready <jbuckmccready@gmail.com>
2025-06-18 09:19:38 -0700

    feat(snacks): filter by cwd for recent picker

--- nvim/.config/nvim/lua/plugins/snacks.lua
+++ nvim/.config/nvim/lua/plugins/snacks.lua
@@ -126,7 +126,7 @@ return {
             { "<leader>ff", function() Snacks.picker.files() end, desc = "Find Files" },
             { "<leader>fg", function() Snacks.picker.git_files() end, desc = "Find Git Files" },
             { "<leader>fp", function() Snacks.picker.projects() end, desc = "Projects" },
-            { "<leader>fr", function() Snacks.picker.recent() end, desc = "Recent" },
+            { "<leader>fr", function() Snacks.picker.recent({ filter = { cwd = true } }) end, desc = "Recent in CWD" },
             -- git related
             { "<leader>gb", function() Snacks.picker.git_branches() end, desc = "Git Branches" },
             { "<leader>gl", function() Snacks.picker.git_log() end, desc = "Git Log" },
