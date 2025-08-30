d2a816f91b0704d3ab66f029069131bf7128e544
Buck McCready <jbuckmccready@gmail.com>
2025-08-30 12:22:51 -0700

    feat(nvim): update plugins (and mini repo)

--- nvim/.config/nvim/lua/plugins/miniai.lua
+++ nvim/.config/nvim/lua/plugins/miniai.lua
@@ -1,5 +1,5 @@
 return {
-    "echasnovski/mini.ai",
+    "nvim-mini/mini.ai",
     lazy = false,
     opts = function()
         local miniai = require("mini.ai")
--- nvim/.config/nvim/lua/plugins/miniclue.lua
+++ nvim/.config/nvim/lua/plugins/miniclue.lua
@@ -1,5 +1,5 @@
 return {
-    "echasnovski/mini.clue",
+    "nvim-mini/mini.clue",
     lazy = false,
     opts = function()
         local miniclue = require("mini.clue")
