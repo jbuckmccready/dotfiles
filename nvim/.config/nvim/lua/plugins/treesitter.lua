b3dcf42624296a08fa34ea20864eb1de0889bf2d
Buck McCready <jbuckmccready@gmail.com>
2025-09-27 22:57:47 -0700

    feat(nvim): update plugins and fix up config for new treesitter to work

--- nvim/.config/nvim/lua/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -1,13 +1,10 @@
 return {
     "nvim-treesitter/nvim-treesitter",
     branch = "main",
-    event = { "BufReadPre", "BufNewFile" },
+    lazy = false,
     build = ":TSUpdate",
-    opts = {
-        highlight = {
-            enable = true,
-        },
-        ensure_installed = {
+    init = function()
+        local parsers_installed = {
             "bash",
             "c",
             "cpp",
@@ -28,6 +25,27 @@ return {
             "vimdoc",
             "yaml",
             "zig",
-        },
-    },
+        }
+
+        vim.defer_fn(function()
+            require("nvim-treesitter").install(parsers_installed)
+        end, 1000)
+        require("nvim-treesitter").update()
+
+        -- auto-start highlights & indentation
+        vim.api.nvim_create_autocmd("FileType", {
+            desc = "User: enable treesitter highlighting",
+            callback = function(ctx)
+                -- start treesitter for highlights
+                -- errors for filetypes with no parser
+                local has_started = pcall(vim.treesitter.start)
+
+                -- indent
+                local no_indent = {}
+                if has_started and not vim.list_contains(no_indent, ctx.match) then
+                    vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+                end
+            end,
+        })
+    end,
 }
