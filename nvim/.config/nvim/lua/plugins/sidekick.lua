d669e22a807248995935c45f096fa6c8d55ce25a
Buck McCready <jbuckmccready@gmail.com>
2025-10-01 12:21:25 -0700

    feat(nvim): update plugins, minor adjustments to sidekick

--- nvim/.config/nvim/lua/plugins/sidekick.lua
+++ nvim/.config/nvim/lua/plugins/sidekick.lua
@@ -3,6 +3,13 @@ return {
     event = "VeryLazy",
     keys = {
         {
+            "<leader>wt",
+            function()
+                require("sidekick.nes").toggle()
+            end,
+            desc = "Toggle Edit Suggestions",
+        },
+        {
             "<tab>",
             function()
                 -- if there is a next edit, jump to it, otherwise apply it if any
@@ -13,8 +20,60 @@ return {
             expr = true,
             desc = "Goto/Apply Next Edit Suggestion",
         },
+        {
+            "<leader>wa",
+            function()
+                require("sidekick.cli").toggle({ focus = true })
+            end,
+            desc = "Toggle AI CLI",
+            mode = { "n", "v" },
+        },
+        {
+            "<leader>wc",
+            function()
+                require("sidekick.cli").toggle({ name = "claude", focus = true })
+            end,
+            desc = "Toggle Claude CLI",
+            mode = { "n", "v" },
+        },
+        {
+            "<leader>wp",
+            function()
+                require("sidekick.cli").prompt()
+            end,
+            desc = "Select AI Prompt",
+            mode = { "n", "v" },
+        },
+        {
+            "<leader>ws",
+            function()
+                require("sidekick.cli").ask({ location = true })
+            end,
+            desc = "Ask AI with context",
+            mode = { "n", "v" },
+        },
+        {
+            "<leader>we",
+            function()
+                require("sidekick.cli").ask({ prompt = "explain", location = true })
+            end,
+            desc = "Ask AI to explain this code",
+            mode = { "n", "v" },
+        },
     },
     opts = {
-        -- using defaults
+        cli = {
+            watch = true, -- watch for file changes in CLI
+            win = {
+                layout = "right",
+                split = {
+                    width = 140,
+                },
+            },
+            mux = {
+                backend = "tmux",
+                enabled = false,
+            },
+        },
     },
 }
